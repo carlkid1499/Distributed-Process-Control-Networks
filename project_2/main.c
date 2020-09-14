86fe3dd6ae37b76dbca146f9baa29daa17215285
Carlos <carlos.santos112@outlook.com>
2020-09-13 20:24:31 -0700

    ISR and Assembly Wrapper
    Got it to work properly.
    Inserted some delays for button reads.
    Need to mess with task priorities to find out what one is the best.

--- project_2/main.c
+++ project_2/main.c
@@ -23,12 +23,14 @@
 #include "CerebotMX7cK.h"
 /*-----------------------------------------------------------*/
 
-void __attribute__( (interrupt(ipl1), vector(_UART2_VECTOR))) vLEDC_ISR_Wrapper( void );
+
 /* ----- Begin: Task Globals ----- */
 static void prvSetupHardware( void );
 static void ToggleLEDB_Task( void *pvParameters );
 static void LEDCHandler_Task( void *pvParameters );
-static void vLEDC_ISR_Handler(void);
+void vLEDC_ISR_Handler(void);
+void __attribute__( (interrupt(IPL1), vector(_CHANGE_NOTICE_VECTOR))) vLEDC_ISR_Wrapper( void );
+void hw_msDelay(unsigned int);
 /* ----- End: Task Globals ----- */
 
 /* ----- Begin: Global handle for Binary Semaphore ----- */
@@ -86,32 +88,25 @@ static void ToggleLEDB_Task( void *pvParameters )
 {
     // local task variables
     TickType_t xLastWakeTick;
-    
+
     for( ;; )
     {
-	if(!(LATB & LEDB))      /* Test for LEDB off */
-	{      
         xLastWakeTick = xTaskGetTickCount(); // Get the the current tick
-        LATBSET = LEDB;     /* Turn LEDB on */
-        vTaskDelayUntil(&xLastWakeTick, pdMS_TO_TICKS(1)); // delay for 1 ms
+        LATBINV = LEDB;     /* Toggle LEDB: by default it is off */
         #if ( configUSE_TRACE_FACILITY == 1 )
             vTracePrint(str, "LEDB set on");
         #endif
-	}
-    else /* LEDB must be on so... */
-    {
-        xLastWakeTick = xTaskGetTickCount(); // Get the the current tick
-        LATBCLR =  LEDB; /* Turn LEDB off */
-        vTaskDelayUntil(&xLastWakeTick, pdMS_TO_TICKS(1)); // delay for 1 ms
-        #if ( configUSE_TRACE_FACILITY == 1 )
-            vTracePrint(str, "LEDB set off");
-        #endif
-    }
+        vTaskDelayUntil(&xLastWakeTick, pdMS_TO_TICKS(1)); // delay for 1 ms	
     }
 }  /* End of prvTestTask1 */
 
 static void LEDCHandler_Task( void *pvParameters)
 {
+    /* As per most tasks, this task is implemented within an infinite loop.
+     * Take the semaphore once to start with so the semaphore is empty before 
+     * the infinite loop is entered.  The semaphore was created before the 
+     * scheduler was started so before this task ran for the first time.*/
+    xSemaphoreTake(LEDC_Semaphore, 0);
     for(;;)
     {
         /* Attempt to get a semaphore. Will block until we get one. */
@@ -121,22 +116,23 @@ static void LEDCHandler_Task( void *pvParameters)
         #if ( configUSE_TRACE_FACILITY == 1 )
             vTracePrint(str, "LEDCHandler_Task received LEDC_Semaphore");
         #endif
-        /* Begin: Button Bounce Trap */
-        while(!PORTReadBits(IOPORT_G,BTN1)); // poll button 1 off
+
         LATBINV = LEDC;
-        vTaskDelay(pdMS_TO_TICKS(20)); // delay for 20 ms
-        while(PORTReadBits(IOPORT_G,BTN1)); // poll button 1 on   
-        /* End: Button Bounce Trap */
     }
 }
 
 void vLEDC_ISR_Handler(void)
 {
     LATBSET = LEDD; // set the LEDD since we entered the ISR
-    portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE; // read more on this..
+    hw_msDelay(20); // 20 ms button debounce
+    while(PORTReadBits(IOPORT_G,BTN1)); // poll button 1 on
+    hw_msDelay(20); // 20 ms button debounce
+    portBASE_TYPE xHigherPriorityTaskWoken = pdTRUE; // read more on this..
     /* Let's give a semaphore to unblock LEDCHandler_Task*/
     xSemaphoreGiveFromISR(LEDC_Semaphore, &xHigherPriorityTaskWoken);
     
+    mCNClearIntFlag(); // Macro function to clear CNI flag
+
     /* Giving the semaphore may have unblocked a task -if it did and the 
      * unblocked task has a priority equal to or above the currently executing 
      * task then xHigherPriorityTaskWoken will have been set to pdTRUE 
@@ -157,22 +153,25 @@ static void prvSetupHardware( void )
     /* Set up PmodSTEM LEDs */
     PORTSetPinsDigitalOut(IOPORT_B, SM_LEDS);
     LATBCLR = SM_LEDS;                      /* Clear all SM LED bits */
-
-/* Enable multi-vector interrupts */
-    INTConfigureSystem(INT_SYSTEM_CONFIG_MULT_VECTOR);  /* Do only once */
-    INTEnableInterrupts();   /*Do as needed for global interrupt control */
-    portENABLE_INTERRUPTS();
     
-//	/* Configure the hardware for maximum performance. */
-//	vHardwareConfigurePerformance();
-//
-//	/* Setup to use the external interrupt controller. */
-//	vHardwareUseMultiVectoredInterrupts();
-//
-//	portDISABLE_INTERRUPTS();
-//
-//	/* Setup the digital IO for the LED's. */
-//	vParTestInitialise();
+    /* ----- BEGIN: CN Interrupts ----- */
+    unsigned int dummy; // used to hold PORT read value
+    // BTN1 and BTN2 pins set for input by Cerebot header file
+    // PORTSetPinsDigitalIn(IOPORT_G, BIT_6 | BIT7); //
+    // Enable CN for BTN1
+    mCNOpen(CN_ON,CN8_ENABLE, 0);
+    // Set CN interrupts priority level 1 sub priority level 0
+    mCNSetIntPriority(1); // Group priority (1 to 7)
+    mCNSetIntSubPriority(0); // Subgroup priority (0 to 3)
+    // read port to clear difference
+    dummy = PORTReadBits(IOPORT_G, BTN1);
+    mCNClearIntFlag(); // Clear CN interrupt flag
+    mCNIntEnable(1); // Enable CN interrupts
+    // Global interrupts must enabled to complete the initialization.
+    /* ----- END: CN Interrupts ----- */
+    
+     // Enable multi vectored interrupts
+    INTEnableSystemMultiVectoredInt(); //done only once    
 }
 /*-----------------------------------------------------------*/
 
@@ -207,20 +206,21 @@ void vApplicationIdleHook( void )
     
     /* Begin: Button Bounce Trap */
     while(!PORTReadBits(IOPORT_G,BTN1)); // poll button 1 off
-    if (!(LATB & LEDA)) // Tests to see if LEDA is off.
-    {
-        LATBSET = LEDA;
-        while(PORTReadBits(IOPORT_G,BTN1)); // poll button 1 on
-        
-    }
-    else
-    {
-        LATBCLR = LEDA;
-        while(PORTReadBits(IOPORT_G,BTN1)); // poll button 1 on
-    }
+    LATBINV = LEDA;
+    hw_msDelay(20); // 20 ms button debounce
+    while(PORTReadBits(IOPORT_G,BTN1)); // poll button 1 on
+    hw_msDelay(20); // 20 ms button debounce
     /* End: Button Bounce Trap */
 }
 /*-----------------------------------------------------------*/
+
+void hw_msDelay(unsigned int mS)
+{
+    unsigned int tWait, tStart;
+    tStart = ReadCoreTimer();         // Read core timer count - SW Start breakpoint
+    tWait = (CORE_MS_TICK_RATE * mS); // Set time to wait
+    while ((ReadCoreTimer() - tStart) < tWait);  // Wait for the time to pass
+}
 
 void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
 {
